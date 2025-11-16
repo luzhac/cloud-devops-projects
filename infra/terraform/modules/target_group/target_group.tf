17831f533724afcca8c7155f5d2b9918765282db
luzhac <luzhac@gmail.com>
2025-11-16 22:17:27 +0000

    a

--- infra/terraform/modules/target_group/target_group.tf
+++ infra/terraform/modules/target_group/target_group.tf
@@ -1,13 +1,13 @@
 resource "aws_lb_target_group" "ingress_http" {
-  name        = "ingress-nodeport-30824"
-  port        = 30824
+  name        = "ingress-nodeport-32457"
+  port        = 32457
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "instance"
 
   health_check {
     path     = "/"
-    port     = "30824"
+    port     = "32457"
     protocol = "HTTP"
   }
 }
@@ -15,5 +15,5 @@ resource "aws_lb_target_group" "ingress_http" {
 resource "aws_lb_target_group_attachment" "ingress_http_attach" {
   target_group_arn = aws_lb_target_group.ingress_http.arn
   target_id        = var.target_id
-  port             = 30824
+  port             = 32457
 }
